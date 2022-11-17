b7bf51dc76181ae7eb71b7c1ca4b6108deb68ff4
Jedddy <jedbalita25@gmail.com>
2022-11-17 20:55:54 +0800

    added fun category

---
+++ bot/cogs/fun.py
@@ -0,0 +1,25 @@
+import discord
+import random
+from discord.ext import commands
+from utils.helper import embed_blueprint
+
+
+class Fun(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+        super().__init__()
+
+    @commands.command(aliases=["8ball"])
+    async def ball(self, ctx: commands.Context):
+        """Makes the bot answer your question"""
+
+        answers = (
+            "No", "Yes", "Maybe", "Ask again next time", "You decide"
+        )
+        embed = embed_blueprint(ctx.guild)
+        embed.description = f"**{random.choice(answers)}**"
+        await ctx.send(embed=embed)
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Fun(bot))
