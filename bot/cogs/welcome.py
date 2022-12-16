0c1d4e554f13bfa64501f911eae89a018ed1cb17
Jedddy <jedbalita25@gmail.com>
2022-12-16 23:21:54 +0800

    added welcomes and insert on verification

--- bot/cogs/welcome.py
+++ bot/cogs/welcome.py
@@ -3,13 +3,14 @@ from discord import (
     Member,
     PartialEmoji,
     RawReactionActionEvent,
-    Role
+    Role,
+    TextChannel
 )
 from discord.ext.commands import Bot, Cog, Context, command
+from databases.moderation_database import ModerationDB
 from config.config import update_config, get_config
 from utils.helper import embed_blueprint
 
-"""TODO: Welcome messages"""
 
 class Welcome(Cog):
     def __init__(self, bot: Bot):
@@ -28,7 +29,7 @@ class Welcome(Cog):
         """Sets verification channel"""
 
         embed = embed_blueprint()
-        embed.description = "**React here to verify**"
+        embed.description = "**React here to verify**\nBy verifiying your account means that you adhere with the server rules and regulations as well as Discord TOS"
         message = await ctx.send(text, embed=embed)
         await update_config(ctx.guild.id, "verificationChannelID", ctx.channel.id)
         await update_config(ctx.guild.id, "verifiedRoleID", int(role.id))
@@ -36,15 +37,21 @@ class Welcome(Cog):
         await message.add_reaction("🇵🇭")
         
     @command()
-    async def setwelcome(self, ctx: Context):
-        pass
+    async def setwelcome(self, ctx: Context, channel: TextChannel = None, *, message: str):
+        """Sets welcome channel with message"""
+
+        channel = channel or ctx.channel
+        await update_config(ctx.guild.id, "welcomeMessage", message)
+        await update_config(ctx.guild.id, "welcomeChannelId", channel.id)
+        await ctx.message.add_reaction('✅')
 
 
 class WelcomeListener(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
         self.server_id = 1031148051760427008 
-        self.emoji = PartialEmoji(name='✅')
+        self.emoji = PartialEmoji(name='🇵🇭')
+        self.db = ModerationDB()
         super().__init__()
 
     @Cog.listener()
@@ -74,14 +81,18 @@ class WelcomeListener(Cog):
         before_role = before.get_role(712536529469440042)
         after_role = after.get_role(712536529469440042)
         if not before_role and after_role:
+            await self.db.insert_member(after.id)
             channel = await after.guild.fetch_channel(645626756295950349)
-            await channel.send(f"Everyone please welcome {after.mention} to our server!")
+            await channel.send(f"Everyone, please welcome {after.mention} to our server!")
 
     @Cog.listener()
-    async def on_member_join(self, member):
-        pass
+    async def on_member_join(self, member: Member):
+        message = await get_config(member.guild.id, "welcomeMessage")
+        channel_id = await get_config(member.guild.id, "welcomeChannelId")
+        channel = member.guild.get_channel(channel_id)
+        await channel.send(message.format(member.mention) if "{}" in message else message)
+
 
 async def setup(bot: Bot):
     await bot.add_cog(Welcome(bot))
     await bot.add_cog(WelcomeListener(bot))
-
